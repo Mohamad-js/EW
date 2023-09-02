0a33e4acefa56938f2dc8f4593ecc4cb0eeba955
Mohamad <mohamadgomar1997@gmail.com>
2023-09-02 18:53:33 +0330

    Header Ready

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger)
+
 const button = document.querySelector('.btn');
 const title = document.querySelector('.title');
 const login = document.querySelector('.login');
@@ -12,11 +14,25 @@ const fader = document.querySelector('.fader');
 const links = document.querySelectorAll('.links');
 const line = document.querySelector('.hover-line');
 const navbar = document.querySelector('.navbar');
+const navHold = document.querySelector('.nav-hold');
 const pic1 = document.querySelector('.n1');
 const pic2 = document.querySelector('.n2');
 const pic3 = document.querySelector('.n3');
 const pic4 = document.querySelector('.n4');
+const back = document.querySelector('.back');
 
+var lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    if (scrollTop > lastScrollTop) {
+        navHold.style.top = "-100px";
+        gsap.to('.toys-drop', {y:-100, ease: 'expo', duration: 0.75, inherit:false});
+        gsap.to('.branch-drop', {y:-100, ease: 'expo', duration: 0.75, inherit:false});
+    } else {
+        navHold.style.top = '0';
+    }
+    lastScrollTop = scrollTop
+})
 
 button.addEventListener('click', (e) => {
 gsap.defaults({duration:2, rotation:100})
@@ -40,8 +56,8 @@ toys.addEventListener('mouseover', () => {
         .to('.toys-drop', {y:400, ease: 'expo', duration:1, inherit:false})
         .to('.branch-drop', {y:0, ease: 'expo', duration:1, inherit:false}, '<')
         fader.style.opacity = '1';
-        toys.style.color = 'white'
-        branches.style.color = 'aqua'
+        toys.style.color = 'aqua'
+        branches.style.color = 'white'
         line.style.right = '183px'
         line.style.width = '102px'
         line.style.opacity = '1'
@@ -50,7 +66,7 @@ toys.addEventListener('mouseover', () => {
 toysDrop.addEventListener('mouseleave', () => {
     gsap.to('.toys-drop', {y:0, ease: 'expo', duration: 0.75, inherit:false});
     fader.style.opacity = '0';
-    toys.style.color = 'aqua'
+    toys.style.color = 'white'
     line.style.opacity = '0'
 });
 
@@ -63,9 +79,9 @@ branches.addEventListener('mouseover', () => {
         .to('.branch-drop', {y:400, ease: 'expo', duration:0.75, inherit:false})
         .to('.toys-drop', {y:0, ease: 'expo', duration: 0.75, inherit:false}, '<');
         fader.style.opacity = '1';
-        branches.style.color = 'white';
-        toys.style.color = 'aqua';
-        about.style.color = 'aqua';
+        branches.style.color = 'aqua';
+        toys.style.color = 'white';
+        about.style.color = 'white';
         line.style.right = '90px'
         line.style.width = '93px'
         line.style.opacity = '1'
@@ -75,7 +91,7 @@ branches.addEventListener('mouseover', () => {
 branchDrop.addEventListener('mouseleave', () => {
     gsap.to('.branch-drop', {y:0, ease: 'expo', duration: 0.75, inherit:false});
     fader.style.opacity = '0';
-    branches.style.color = 'aqua'
+    branches.style.color = 'white'
     line.style.opacity = '0'
 });
 
@@ -85,8 +101,8 @@ branchDrop.addEventListener('mouseover', () => {
 
 about.addEventListener('mouseover', () => {
     gsap.to('.branch-drop', {y:0, ease: 'expo', duration:1, inherit:false})
-    about.style.color = 'white';
-    branches.style.color = 'aqua';
+    about.style.color = 'aqua';
+    branches.style.color = 'white';
     fader.style.opacity = '0';
     line.style.width = '90px';
     line.style.right = '0';
@@ -94,7 +110,7 @@ about.addEventListener('mouseover', () => {
 })
 
 about.addEventListener('mouseleave', () => {
-    about.style.color = 'aqua'; 
+    about.style.color = 'white'; 
 })
 
 navbar.addEventListener('mouseleave', () => {
@@ -104,30 +120,31 @@ navbar.addEventListener('mouseleave', () => {
 rep.addEventListener('mouseover', () => {
     gsap.to('.toys-drop', {y:0, ease: 'expo', duration: 0.75, inherit:false});
     fader.style.opacity = '0';
-    toys.style.color = 'aqua'
+    toys.style.color = 'white'
     line.style.width = '159px';
     line.style.right = '285px';
     line.style.opacity = '1';
-    rep.style.color = 'white'
-})
-
-rep.addEventListener('mouseout', () => {
     rep.style.color = 'aqua'
 })
 
+rep.addEventListener('mouseout', () => {
+    rep.style.color = 'white'
+})
+
 account.addEventListener('mouseover', () => {
-    account.style.color = 'white'
+    account.style.color = 'aqua'
     line.style.right = '444px';
     line.style.opacity = '1';
     line.style.width = '128px';
 })
 
 account.addEventListener('mouseout', () => {
-    account.style.color = 'aqua';
+    account.style.color = 'white';
 })
 
-gsap.timeline({repeat:-1, repeatDelay:5})
+const wallpaper = gsap.timeline({repeat:-1, repeatDelay:5})
     .fromTo('.n4', {x:-1500}, {x:0, ease:'expo', zIndex:-4})
+    .fromTo('.text1', {opacity:1}, {opacity:0, zIndex:1}, 5)
     .fromTo('.n3', {x:-1500}, {x:0, ease:'expo', zIndex:-3}, 5)
     .fromTo('.text2', {opacity:0}, {opacity:1, zIndex:1}, 5)
     .fromTo('.text2', {opacity:1}, {opacity:0, zIndex:1}, 10)
@@ -136,3 +153,11 @@ gsap.timeline({repeat:-1, repeatDelay:5})
     .fromTo('.text3', {opacity:1}, {opacity:0, zIndex:1}, 15)
     .fromTo('.n1', {x:-1500}, {x:0, ease:'expo', zIndex:-1}, 15)
     .fromTo('.text4', {opacity:0}, {opacity:1, zIndex:1}, 15)
+
+back.addEventListener('mouseover', () => {
+    wallpaper.pause();
+})
+
+back.addEventListener('mouseout', () => {
+    wallpaper.resume();
+})
